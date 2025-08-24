0ea5f6346fe9867162b724d76a66daa621c70f34
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-24 15:13:48 +0200

    unzipgit

---
+++ assets/shared/data/scrapped/hud.lua
@@ -0,0 +1,9 @@
+function onCreatePost()
+  doTweenColor('scoreTxt', 'scoreTxt', 'ff53b7', 0.4, 'linear');
+  doTweenColor('timeBar', 'timeBar', 'ff53b7', 0.4, 'linear');
+		
+    setProperty('gf.visible', false);
+    setTextFont('scoreTxt', 'Gumball.TTF');
+    setTextFont('timeTxt', 'Gumball.TTF');
+    end
+--By hyperlios
