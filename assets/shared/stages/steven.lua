adca3c5a81161c6d6a0d9b11b4bb89f5f6aaf11f
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-26 18:41:55 +0200

    unzipgit

---
+++ assets/shared/stages/steven.lua
@@ -0,0 +1,6 @@
+function onCreate()
+addLuaScript('scripts extras/HealthBar Steven')
+makeLuaSprite('Fortnite_battle_passxd', 'bgs/Steven', -875,-610)
+scaleObject('Fortnite_battle_passxd', 4.0,4.0)
+addLuaSprite('Fortnite_battle_passxd', false)
+end
