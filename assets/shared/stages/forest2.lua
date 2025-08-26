525f3eb341f17c7c72d95a479bcafad5d9e0ebef
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-26 18:41:59 +0200

    unzipgit

---
+++ assets/shared/stages/forest2.lua
@@ -0,0 +1,9 @@
+function onCreate()
+addLuaScript('scripts extras/healtbar2 by AnsfoxyMB')
+makeLuaSprite('Fortnite_battle_passxd', 'bgs/regular show/bg3', -775,-360)
+scaleObject('Fortnite_battle_passxd', 5.6,5.6)
+addLuaSprite('Fortnite_battle_passxd', false)
+makeLuaSprite('Fortnite_battle_pass', 'bgs/regular show/bg2', -3075,-360)
+scaleObject('Fortnite_battle_pass', 4.8,4.8)
+addLuaSprite('Fortnite_battle_pass', false)
+end
